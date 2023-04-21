f82c5d2f04f0c1c390c1fbbfb96aaa89da80ee1f
Alex <alex@stsolution.be>
2023-04-21 11:14:50 +0200

    fist commit

---
+++ src/main/java/com/example/projint2/Projint2Application.java
@@ -0,0 +1,13 @@
+package com.example.projint2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projint2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projint2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projint2/Projint2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projint2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projint2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
